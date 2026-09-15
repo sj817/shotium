e74278cf87588bb80d945073aa45a4e00c9a119c
sj817 <74231782+sj817@users.noreply.github.com>
2026-09-15 18:51:24 +0800

    cut(deps): drop third_party/metrics_proto and remove uncalled UkmSource::PopulateProto

--- services/metrics/public/cpp/ukm_source.h
+++ services/metrics/public/cpp/ukm_source.h
@@ -16,8 +16,6 @@
 
 namespace ukm {
 
-class Source;
-
 // Contains UKM URL data for a single source id.
 class METRICS_EXPORT UkmSource {
  public:
@@ -133,9 +131,6 @@ class METRICS_EXPORT UkmSource {
 
   // Sets the resolved URLs.
   void set_resolved_urls(const std::vector<GURL>& urls);
-
-  // Serializes the members of the class into the supplied proto.
-  void PopulateProto(Source* proto_source) const;
 
   // Sets the current "android_activity_type" state.
   static void SetAndroidActivityTypeState(int32_t android_activity_type);
